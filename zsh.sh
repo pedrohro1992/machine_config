758d3a5ad506b857262627b30c0a012621fb585b
Pedro Oliveira <pedro_hro1992@outlook.com>
2023-03-05 17:22:23 -0300

    Add new k8s conf and ASDF plugins

--- zsh.sh
+++ zsh.sh
@@ -24,12 +24,17 @@ mv zshrc ~/.zshrc
 
 #Stern ASDF Plugins
 
-asdf plugin add  kubectl 
+asdf plugin add kubectl 
 asdf plugin add terraform
 asdf plugin add golang
 asdf plugin add kops
 asdf plugin add python
 asdf plugin add stern 
+asdf plugin add kubectx
+asdf plugin add helmfile
+asdf plugin add eksctl
+asdf plugin add terragrunt
+
 
 
 #Install All the Latest Version
@@ -41,13 +46,23 @@ asdf install golang latest
 asdf install kops latest
 asdf install python latest
 asdf install stern latest
+asdf install kubectx latest 
+asdf install helmfile latest
+asdf install eksctl latest
+asdf install terragrunt latest
+
 
 
 #Set The global off all plugins to latest
 
 #asdf global kubectl latest
 asdf global terraform latest
+asdf global kubectl latest
 asdf global golang latest
 asdf global kops latest
 asdf global python latest
 asdf global stern latest
+asdf global kubectx latest
+asdf global helmfile latest
+asdf global eksctl latest
+asdf global terragrunt latest
