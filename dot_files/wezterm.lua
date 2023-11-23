53e732cbe3cd732ff9f6b25f1d56d8474c61e0dc
Pedro Oliveira <phrdoliveira@rd.com.br>
2023-11-23 12:18:02 -0300

    update Wezterm config file

--- dot_files/wezterm.lua
+++ dot_files/wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
 
 -- This table will hold the configuration.
 local config = {}
@@ -7,33 +7,56 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
-
+--KeyMaps
+config.leader = { key = 'a', mods = 'CTRL' }
 config.keys = {
-	{
-		key = "f",
-		mods = "CTRL|ALT",
-		action = wezterm.action.ToggleFullScreen,
-	},
-	{
-		key = "e",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	{
-		key = "o",
-		mods = "CTRL|SHIFT",
-		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
+  -- Enter Full Scren mode
+  {
+    key = 'f',
+    mods = 'ALT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+  {
+    key = 'e',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'o',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  { 
+    key = 'F9', 
+    mods = 'ALT', 
+    action = wezterm.action.ShowTabNavigator 
+  },
+  { 
+   key = 'h',
+   mods = 'CTRL|SHIFT',
+   action = wezterm.action.Hide
+  },
+  {
+    key = 'a',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.AdjustPaneSize { 'Down', 15 }
+  },
+
 }
 
-config.color_scheme = "Andromeda"
-config.font = wezterm.font("UbuntuMono Nerd Font")
-config.font_size = 13.0
-config.default_domain = "WSL:Ubuntu"
-config.window_background_opacity = 0.5
-config.text_background_opacity = 0.5
+-- Set the default domain to WSL Ubuntu 
+--  config.default_domain = 'WSL:Ubuntu'
+-- Apperance
+-- Set the theme
+config.color_scheme = 'One Dark (Gogh)'
+-- Set opacity
+config.window_background_opacity = 1.0
+config.text_background_opacity = 1.0
+-- Font Conf
+config.font = wezterm.font 'Hack Nerd Font'
+config.font_size = 10
+-- return the config to wezterm
+return config 
 
--- and finally, return the configuration to wezterm
-return config
