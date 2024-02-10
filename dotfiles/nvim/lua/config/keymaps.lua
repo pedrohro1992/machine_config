f89b9a1130b131a40bc436527dc7d3259a147de0
Pedro Oliveira <pedro_hro1992@outlook.com>
2024-02-10 13:31:58 -0300

    Add better vim keymap to split window on vertical

--- dotfiles/nvim/lua/config/keymaps.lua
+++ dotfiles/nvim/lua/config/keymaps.lua
@@ -18,9 +18,9 @@ vim.keymap.set(
 vim.keymap.set("i", "jj", "<esc>")
 
 -- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+vim.keymap.set("n", "<leader>wv", "<C-w>v") -- split window vertically
 vim.keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
+vim.keymap.set("n", "<leader>we", "<C-w>=") -- make split windows equal width & height
 vim.keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 -------------------
 --Plugins keybinds
