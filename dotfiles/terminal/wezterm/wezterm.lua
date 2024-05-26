aa725dcc3df3199bb2c0f08896f6132cf74b919b
Pedro Oliveira <pedro_hro1992@outlook.com>
2024-05-26 20:29:10 -0300

    Adiciona pligins pro asdf e atualiza wezterm

--- dotfiles/terminal/wezterm/wezterm.lua
+++ dotfiles/terminal/wezterm/wezterm.lua
@@ -3,6 +3,10 @@ local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {}
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
@@ -22,13 +26,18 @@ config.keys = {
 -- Set the default domain to WSL Ubuntu
 -- config.default_domain = 'WSL:Ubuntu'
 -- Apperance
+-- Window and Tabs
+config.window_decorations = "RESIZE"
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.hide_tab_bar_if_only_one_tab = true
 -- Set the theme
 config.color_scheme = 'Kanagawa (Gogh)'
-config.window_background_opacity = 0.9
---config.text_background_opacity = 0.9
+config.window_background_opacity = 0.8
+-- config.text_background_opacity = 1.0
 -- Font Conf
-config.font = wezterm.font("CaskaydiaMono Nerd Font") 
+config.font = wezterm.font("CaskaydiaMono Nerd Font")
 config.font_size = 11
 -- return the config to wezterm
---config.enable_wayland = true
+config.enable_wayland = false
 return config
